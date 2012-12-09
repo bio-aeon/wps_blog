2fc031ab3bfad9635a9a7d94ce8d7f3952eae6e6
Alexandr <chigrinets.alexandr@gmail.com>
2012-12-09 20:43:08 +0600

    fancybox

--- wpssite/urls.py
+++ wpssite/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 admin.autodiscover()
 
@@ -13,3 +15,5 @@ urlpatterns += patterns('',
     url(r'^page/', include('django.contrib.flatpages.urls')),
     url(r'^djadmin/', include(admin.site.urls)),
 )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
