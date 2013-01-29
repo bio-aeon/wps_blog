5457ed3e9e68206cce1f24c6c33a9a60c8f05094
Alexandr <chigrinets.alexandr@gmail.com>
2013-01-30 00:04:50 +0600

    disc generator using

--- wpssite/blog/views.py
+++ wpssite/blog/views.py
@@ -91,9 +91,7 @@ class Comments(CallableClassView):
                                                       'id': comment.id,
                                                       'result': 'success'}))
             else:
-                errors = {}
-                for k in form.errors:
-                    errors[k] = form.errors[k][0]
+                errors = {k: form.errors[k][0] for k in form.errors}
                 return HttpResponse(simplejson.dumps({'data': errors, 'result': 'error'}))
         else:
             raise Http404
