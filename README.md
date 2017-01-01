ee3540cd9772cb12b12770a02b53701859a18e7d
Alexandr <alexandr@wps.su>
2017-01-02 00:14:38 +0300

    Adding some smoke tests for the blog app

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
+# Wps Blog
+
 My simple blog on Django with search, tags and tree comments with rating.
+
+## Run locally
+
+```bash
+pip install -r requirements.txt
+python manage.py migrate --settings=wps_blog.settings --configuration=Dev
+python manage.py runserver --settings=wps_blog.settings --configuration=Dev
+```
+
+## Run tests
+
+```bash
+python manage.py test --settings=wps_blog.settings --configuration=Dev
+```
--- blog/tests.py
+++ blog/tests.py
@@ -1,16 +1,15 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+# -*- coding: utf-8 -*-
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class PostsViewTests(TestCase):
+    def test_start(self):
+        response = self.client.get(reverse('posts-start'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, u'Блог | Web and programming solutions')
+
+    def test_index(self):
+        response = self.client.get(reverse('posts-index'))
+        self.assertEqual(response.status_code, 200)
