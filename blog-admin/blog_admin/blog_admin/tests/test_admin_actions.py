632ea527d2dad58303131c793129fc003c89c218
Alexandr Chigrinets <alexandr@wps.su>
2026-02-23 22:42:22 +0200

    Test(Admin): adding draft/publish workflow tests

--- blog-admin/blog_admin/blog_admin/tests/test_admin_actions.py
+++ blog-admin/blog_admin/blog_admin/tests/test_admin_actions.py
@@ -79,3 +79,66 @@ class CommentModerationTest(TestCase):
             {'post': 'yes'}
         )
         self.assertFalse(Comment.objects.filter(pk=reply.pk).exists())
+
+
+class PublishWorkflowTest(TestCase):
+    def setUp(self):
+        self.admin_user = User.objects.create_superuser('admin', 'a@b.com', 'pass')
+        self.client.force_login(self.admin_user)
+        self.draft_post = baker.make(Post, author=self.admin_user, is_hidden=True)
+        self.published_post = baker.make(Post, author=self.admin_user, is_hidden=False)
+
+    def test_new_post_defaults_to_draft(self):
+        self.assertTrue(Post._meta.get_field('is_hidden').default)
+
+    def test_publish_action_sets_is_hidden_false(self):
+        self.client.post(
+            reverse('admin:blog_admin_post_changelist'),
+            {
+                'action': 'publish_posts',
+                '_selected_action': [self.draft_post.pk],
+            }
+        )
+        self.draft_post.refresh_from_db()
+        self.assertFalse(self.draft_post.is_hidden)
+
+    def test_unpublish_action_sets_is_hidden_true(self):
+        self.client.post(
+            reverse('admin:blog_admin_post_changelist'),
+            {
+                'action': 'unpublish_posts',
+                '_selected_action': [self.published_post.pk],
+            }
+        )
+        self.published_post.refresh_from_db()
+        self.assertTrue(self.published_post.is_hidden)
+
+    def test_bulk_publish_multiple_posts(self):
+        draft2 = baker.make(Post, author=self.admin_user, is_hidden=True)
+        self.client.post(
+            reverse('admin:blog_admin_post_changelist'),
+            {
+                'action': 'publish_posts',
+                '_selected_action': [self.draft_post.pk, draft2.pk],
+            }
+        )
+        self.draft_post.refresh_from_db()
+        draft2.refresh_from_db()
+        self.assertFalse(self.draft_post.is_hidden)
+        self.assertFalse(draft2.is_hidden)
+
+    def test_filter_by_draft_status(self):
+        response = self.client.get(
+            reverse('admin:blog_admin_post_changelist') + '?is_hidden__exact=1'
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.draft_post.name)
+        self.assertNotContains(response, self.published_post.name)
+
+    def test_filter_by_published_status(self):
+        response = self.client.get(
+            reverse('admin:blog_admin_post_changelist') + '?is_hidden__exact=0'
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.published_post.name)
+        self.assertNotContains(response, self.draft_post.name)
